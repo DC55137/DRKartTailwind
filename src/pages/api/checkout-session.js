dae9d81019c00d716482964e6203393c781ff612
cb roofing <cbroofing@MacBook-Pro.modem>
2023-02-13 17:22:24 +1000

    please x6

--- src/pages/api/checkout-session.js
+++ src/pages/api/checkout-session.js
@@ -19,6 +19,8 @@ export default async function handler(req, res) {
         automatic_tax: { enabled: true },
       })
       res.setHeader('Access-Control-Allow-Origin', '*')
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
       res.redirect(303, session.url)
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message)
